299a6c518f2a30d43fd3935d4d8f963882c0432c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-23 17:45:12 +0300

    working on heap and heap sorts

--- HeapsAndHeapSort/src/HeapOperations.java
+++ HeapsAndHeapSort/src/HeapOperations.java
@@ -1,10 +1,25 @@
 import java.util.List;
 
 public class HeapOperations {
-    public List<Integer> siftDownHeapfication(List<Integer> nums){
+    public List<Integer> siftDownHeapification(List<Integer> nums){
 
+        int end = nums.size()-1;
 
+        while(end >= 0){
+            int parent = (end - 1)/2;
+
+            if(nums.get(parent) > nums.get(end)){
+                swap(nums,parent,end);
+            }
+            --end;
+        }
 
         return nums;
+    }
+
+    private void swap(List<Integer> nums, int a, int b){
+        int temp = nums.get(a);
+        nums.set(a, nums.get(b));
+        nums.set(b,temp);
     }
 }
--- HeapsAndHeapSort/src/Main.java
+++ HeapsAndHeapSort/src/Main.java
@@ -1,7 +1,5 @@
-import java.nio.charset.IllegalCharsetNameException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -9,7 +7,7 @@ public class Main {
         HeapOperations heapOperations = new HeapOperations();
 
         List<Integer> nums = new ArrayList<>(Arrays.asList(1,7,30,13,26,15,2));
-        nums = heapOperations.siftDownHeapfication(nums);
+        nums = heapOperations.siftDownHeapification(nums);
         for (int i : nums){
             System.out.print(i+",\t");
         }
