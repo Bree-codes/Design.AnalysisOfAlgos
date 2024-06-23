0c8fbdbcdba5f143c1890fa7f90286708adbf2ab
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-23 17:48:17 +0300

    working on heap and heap sorts

--- HeapsAndHeapSort/src/Main.java
+++ HeapsAndHeapSort/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
         HeapOperations heapOperations = new HeapOperations();
 
-        List<Integer> nums = new ArrayList<>(Arrays.asList(1,7,30,13,26,15,2));
+        List<Integer> nums = new ArrayList<>(Arrays.asList(15,16,1,30,0,5,20));
         nums = heapOperations.siftDownHeapification(nums);
         for (int i : nums){
             System.out.print(i+",\t");
