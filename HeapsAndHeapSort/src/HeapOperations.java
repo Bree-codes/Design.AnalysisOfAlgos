9bcaaa84a9b48091764ddcc5440b7ce24e13e9ef
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-23 17:23:55 +0300

    working on heap and heap sorts

--- HeapsAndHeapSort/src/HeapOperations.java
+++ HeapsAndHeapSort/src/HeapOperations.java
@@ -1,2 +1,10 @@
+import java.util.List;
+
 public class HeapOperations {
+    public List<Integer> siftDownHeapfication(List<Integer> nums){
+
+
+
+        return nums;
+    }
 }
--- HeapsAndHeapSort/src/Main.java
+++ HeapsAndHeapSort/src/Main.java
@@ -1,5 +1,17 @@
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        HeapOperations heapOperations = new HeapOperations();
+
+        List<Integer> nums = new ArrayList<>(Arrays.asList(1,7,30,13,26,15,2));
+        nums = heapOperations.siftDownHeapfication(nums);
+        for (int i : nums){
+            System.out.print(i+",\t");
+        }
     }
 }
