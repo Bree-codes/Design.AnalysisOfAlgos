de041f72b8bae110f509538cc45fdc64a9613b9d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-23 12:37:03 +0300

    swapping algo

---
+++ SimpleSwapping/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int a = 0, b=1;
+
+        swap(arr,a,b);
+
+        System.out.println("The value at index 0 is: " + arr[0]);
+        System.out.println("The value at index 1 is: " + arr[b]);
+
+    }
+}
