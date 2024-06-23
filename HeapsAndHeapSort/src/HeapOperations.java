c46e61a70019db6b66dc53cb7cb66e2fa0af5e9e
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-23 18:33:34 +0300

    working on heap and heap sorts

--- HeapsAndHeapSort/src/HeapOperations.java
+++ HeapsAndHeapSort/src/HeapOperations.java
@@ -4,16 +4,42 @@ public class HeapOperations {
     public List<Integer> siftDownHeapification(List<Integer> nums){
 
         int end = nums.size()-1;
+        final int LENGTH = end;
 
         while(end >= 0){
             int parent = (end - 1)/2;
 
             if(nums.get(parent) > nums.get(end)){
                 swap(nums,parent,end);
+
+                int pointer = end;
+
+                int left_child = 2 * pointer + 1;
+                int right_child = left_child + 1;
+
+                while(left_child <= LENGTH){
+                    if(right_child <= LENGTH){
+                        if(nums.get(left_child) <= nums.get(right_child) && nums.get(left_child) < nums.get(pointer)){
+                            swap(nums, left_child, pointer);
+                            pointer = left_child;
+                        } else if(nums.get(right_child) < nums.get(pointer)) {
+                            swap(nums, right_child, pointer);
+                            pointer = right_child;
+                        }else {
+                            break;
+                        }
+                    } else if (nums.get(left_child) < nums.get(pointer)) {
+                        swap(nums, left_child, pointer);
+                        pointer = left_child;
+                    }else {
+                        break;
+                }
+                    left_child = 2 * pointer + 1;
+                    right_child = left_child + 1;
             }
+        }
             --end;
         }
-
         return nums;
     }
 
--- HeapsAndHeapSort/src/Main.java
+++ HeapsAndHeapSort/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
         HeapOperations heapOperations = new HeapOperations();
 
-        List<Integer> nums = new ArrayList<>(Arrays.asList(15,16,1,30,0,5,20));
+        List<Integer> nums = new ArrayList<>(Arrays.asList(15,16,1,30,0,5,20,3,1,2,7,64,33,7,6,54,65,44,3,6,22,66,25,64,12,554,12,443,44,11,66,14,12,6,12,658,8,87,6));
         nums = heapOperations.siftDownHeapification(nums);
         for (int i : nums){
             System.out.print(i+",\t");
